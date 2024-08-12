3281c08f8726a648bfcebed1f0b7cdea9b84596b
hoangson <lehoangsonsg@gmail.com>
2024-08-12 20:41:36 +0930

    feat: add exception handlers

--- src/asgi/app.py
+++ src/asgi/app.py
@@ -3,9 +3,13 @@ from litestar import Litestar
 from src.asgi.plugins import alchemy
 from src.controller.user.router import UserController
 from src.utils.dependencies import create_collection_dependencies
+from src.utils.exceptions import exception_to_http_response
 
 app = Litestar(
     route_handlers=[UserController],
     plugins=[alchemy],
     dependencies=create_collection_dependencies(),
+    exception_handlers={
+        Exception: exception_to_http_response,
+    },
 )
