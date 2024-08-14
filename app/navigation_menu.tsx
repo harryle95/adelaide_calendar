c169d2d09f3a7b025de47ea3598fb00fffee5374
harryle <lehoangsonsg@gmail.com>
2024-08-14 21:01:42 +0930

    feat: add university logo to menu

--- app/navigation_menu.tsx
+++ app/navigation_menu.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 const menuMap: Record<string, string> = {
   Home: "/",
@@ -8,17 +9,26 @@ const menuMap: Record<string, string> = {
 };
 
 const NavigationMenu = React.memo(() => {
+  const linkClassName = "flex h-full items-center hover:bg-slate-600 px-2";
   return (
-    <div className="flex h-16 w-full bg-slate-900 px-5">
-      <div className="flex flex-grow items-center gap-x-4 text-lg font-bold text-white">
+    <div className="flex h-16 w-full bg-slate-900 px-5 text-lg font-bold text-white">
+      <div className="flex flex-grow items-center gap-x-4">
+        <Image
+          src="/adelaide_logo.png"
+          width={50}
+          height={50}
+          alt="Adelaide University Logo"
+        />
         {Object.entries(menuMap).map(([title, ref]) => (
-          <div
-            key={title}
-            className="flex h-full items-center hover:bg-slate-600"
-          >
-            <a href={ref}>{title}</a>
-          </div>
+          <a href={ref} key={title} className={linkClassName}>
+            {title}
+          </a>
         ))}
+      </div>
+      <div>
+        <a href="#" className={linkClassName}>
+          Login
+        </a>
       </div>
     </div>
   );
