e9b182fd8b6117c74c822a636c663d5269061fd0
hoangson <lehoangsonsg@gmail.com>
2024-08-20 23:20:02 +0930

    WIP: setup googleoauth

---
+++ app/auth/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <form action="/api/authorise">
+      <button type="submit">Login</button>
+    </form>
+  );
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: "http://127.0.01:8080/:path*"
+            }
+        ]
+    }
+};
 
 export default nextConfig;
