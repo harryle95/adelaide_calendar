f765567631ad726065a73c85c1a92822e3f4eb2a
harryle <lehoangsonsg@gmail.com>
2024-08-26 08:47:32 +0930

    feat: add update method, rework some auth logics

--- src/db/models/user.py
+++ src/db/models/user.py
@@ -10,9 +10,9 @@ class User(UUIDAuditBase):
     __table_args__ = {"comment": "User accounts for application access"}
     __pii_columns__ = {"name", "email", "avatar_url"}
 
-    email: Mapped[str] = mapped_column(unique=True, index=True, nullable=False)
-    name: Mapped[str | None] = mapped_column(nullable=True, default=None)
-    hashed_password: Mapped[str | None] = mapped_column(String(length=255), nullable=True, default=None)
+    email: Mapped[str | None] = mapped_column(unique=True, index=True, nullable=True)
+    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(length=255))
     avatar_url: Mapped[str | None] = mapped_column(String(length=500), nullable=True, default=None)
     is_superuser: Mapped[bool] = mapped_column(default=False, nullable=False)
     is_verified: Mapped[bool] = mapped_column(default=False, nullable=False)
