451ec3566f80f9b3211925e2e01499aa37719561
harryle <lehoangsonsg@gmail.com>
2024-08-25 20:37:08 +0930

    feat: implement login, register and forgot password pages - no handling logic #26

--- app/routes/auth/index.tsx
+++ app/routes/auth/index.tsx
@@ -1,6 +1,16 @@
 import { RouteObject } from "react-router-dom";
-import Page from "./page";
+import Page, { ForgotPasswordForm, LoginForm, SignUpForm } from "./page";
 
-const route: RouteObject[] = [{ path: "/auth", element: <Page /> }];
+const route: RouteObject[] = [
+  {
+    path: "/auth",
+    element: <Page />,
+    children: [
+      { path: "/auth", element: <LoginForm /> },
+      { path: "/auth/signup", element: <SignUpForm /> },
+      { path: "/auth/forgotPassword", element: <ForgotPasswordForm /> },
+    ],
+  },
+];
 
 export { route };
--- app/routes/navigation_menu.tsx
+++ app/routes/navigation_menu.tsx
@@ -14,7 +14,7 @@ const NavigationMenu = React.memo(() => {
       ? "flex h-full items-center bg-slate-600 px-4"
       : "flex h-full items-center hover:bg-slate-600 px-4";
   return (
-    <div className="flex h-16 w-full bg-slate-900 px-5 text-lg font-bold text-white shadow-lg shadow-slate-400">
+    <div className="flex flex-shrink-0 h-16 w-full bg-slate-900 px-5 text-lg font-bold text-white shadow-lg shadow-slate-400">
       <div className="flex flex-grow items-center">
         <img src="/vite.svg" alt="Vite Logo" className="h-full w-auto p-2" />
         {Object.entries(menuMap).map(([title, ref]) => (
