4d3178e7f7c26f72508c258cfab1d5bd81a4c824
hoangson <lehoangsonsg@gmail.com>
2024-08-28 08:25:13 +0930

    feat: add profile page

--- app/routes/auth/page.tsx
+++ app/routes/auth/page.tsx
@@ -3,21 +3,22 @@ import * as Form from "./form";
 import { Link } from "react-router-dom";
 import { useLoaderData } from "react-router-dom";
 import type { SCHEMA } from "../../service";
+import { ExclamationTriangleIcon } from "@radix-ui/react-icons";
 
-const rootClass = "p-7 rounded-md flex flex-col bg-white";
+const rootClass =
+  "p-7 rounded-md flex flex-col bg-white min-w-[500px] min-h-[400px] justify-between";
 const headerClass = "flex justify-center items-center font-bold text-3xl p-6";
-const bodyClass = "flex flex-col gap-y-3";
-const footerClass =
-  "mt-3 flex items-center justify-center max-w-[300px] text-center";
+const bodyClass = "flex flex-col gap-y-4";
+const footerClass = "mt-3 flex items-center justify-center text-center";
 const labelClass = "font-medium flex justify-between";
-const inputClass = "px-2 py-1 rounded-md border-2 w-[300px]";
+const inputClass = "px-2 py-1 rounded-md border-2 w-full";
 const buttonClass =
   "mt-3 rounded-2xl px-4 py-2 fond-extrabold bg-blue-500 text-white";
 const linkClass = "text-blue-700 font-medium";
 
 function SignUpForm() {
   return (
-    <Form.Root className={rootClass}>
+    <>
       <Form.Header className={headerClass} title="Sign-Up" />
       <Form.Body className={bodyClass} method="POST">
         <Form.InputGroup
@@ -37,13 +38,13 @@ function SignUpForm() {
           Sign-up
         </button>
       </Form.Body>
-    </Form.Root>
+    </>
   );
 }
 
 function LoginForm() {
   return (
-    <Form.Root className={rootClass}>
+    <>
       <Form.Header className={headerClass} title="Login" />
       <Form.Body className={bodyClass} method="POST">
         <Form.InputGroup
@@ -81,13 +82,13 @@ function LoginForm() {
           </div>
         </div>
       </Form.Footer>
-    </Form.Root>
+    </>
   );
 }
 
 function ForgotPasswordForm() {
   return (
-    <Form.Root className={rootClass}>
+    <>
       <Form.Header className={headerClass} title="Reset Password" />
 
       <Form.Body className={bodyClass}>
@@ -106,13 +107,13 @@ function ForgotPasswordForm() {
           A password reset email will be sent to your registered email.
         </div>
       </Form.Footer>
-    </Form.Root>
+    </>
   );
 }
 
 function ChangePasswordForm() {
   return (
-    <Form.Root className={rootClass}>
+    <>
       <Form.Header className={headerClass} title="Change Password" />
 
       <Form.Body className={bodyClass}>
@@ -136,20 +137,33 @@ function ChangePasswordForm() {
           Submit
         </button>
       </Form.Body>
-    </Form.Root>
+    </>
   );
 }
 
 function Profile() {
   const profile = useLoaderData() as SCHEMA["User"];
   const displayName = profile.name ? profile.name : profile.email;
-  return <div>Welcome back {displayName}</div>;
+  return (
+    <div className="flex flex-col">
+      <h1 className={headerClass}>Welcome back {displayName}</h1>
+
+      {!profile.isVerified && (
+        <p className="bg-red-500 text-white p-4 rounded-md flex justify-between items-center gap-x-3">
+          <ExclamationTriangleIcon />
+          Please verify your account by linking an email address!
+        </p>
+      )}
+    </div>
+  );
 }
 
 function Page() {
   return (
     <div className="h-full w-full flex justify-center items-center">
-      <Outlet />
+      <Form.Root className={rootClass}>
+        <Outlet />
+      </Form.Root>
     </div>
   );
 }
