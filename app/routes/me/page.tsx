b860e1bb4142710525fa417902b6143db8c37d73
hoangson <lehoangsonsg@gmail.com>
2024-08-28 07:46:28 +0930

    feat: move profile page to be part of auth

--- app/routes/me/page.tsx
+++
@@ -1,9 +0,0 @@
-import { useLoaderData } from "react-router-dom";
-
-function Page() {
-  const profile = useLoaderData();
-  console.log(profile);
-  return <div>Profile</div>;
-}
-
-export { Page };
