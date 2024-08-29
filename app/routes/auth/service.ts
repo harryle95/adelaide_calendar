e828ddde0ba414e9462d648fbba7c75d5e3ce0d0
harryle <lehoangsonsg@gmail.com>
2024-08-29 20:54:45 +0930

    feat: add style css

--- app/routes/auth/service.ts
+++ app/routes/auth/service.ts
@@ -1,27 +1,26 @@
 import { redirect } from "react-router-dom";
 import type { SCHEMA } from "../../service";
 import { UserRepository, MeRepository } from "../../service";
-
-function formDataToObject(data: FormData) {
-  const result: { [key: string]: FormDataEntryValue } = {};
-  data.forEach((value, key) => (result[key] = value));
-  return result;
-}
+import { defer } from "react-router-dom";
 
 const UserService = {
   RegisterUser: async ({ request }: { request: Request }) => {
-    const data = formDataToObject(
-      await request.formData()
-    ) as SCHEMA["UserCreate"];
-    await UserRepository.RegisterUser(data);
-    return redirect("/me");
+    const data = await request.json();
+    try {
+      await UserRepository.RegisterUser(data);
+      return redirect("/auth/me");
+    } catch (error) {
+      return error;
+    }
   },
   LoginUser: async ({ request }: { request: Request }) => {
-    const data = formDataToObject(
-      await request.formData()
-    ) as SCHEMA["UserLogin"];
-    await UserRepository.LoginUser(data);
-    return redirect("/me");
+    const data = await request.json();
+    try {
+      await UserRepository.LoginUser(data);
+      return redirect("/auth/me");
+    } catch (error) {
+      return error;
+    }
   },
   LogoutUser: async () => {
     await UserRepository.LogoutUser();
@@ -33,20 +32,21 @@ const MeService = {
   GetMe: async () => {
     try {
       const result = await MeRepository.GetMe();
-      return result.data;
+      return defer({ user: result.data });
     } catch (error) {
       console.log(error);
-      return redirect("/auth/");
+      return { user: { id: null, name: null, email: null } };
     }
   },
   UpdateMe: async (data: SCHEMA["UserUpdate"]) => {
     await MeRepository.UpdateMe(data);
     return redirect("/");
   },
-  UpdateMyPassword: async (data: SCHEMA["UserChangePassword"]) => {
+  UpdateMyPassword: async ({ request }: { request: Request }) => {
+    const data: SCHEMA["UserChangePassword"] = await request.json();
     await MeRepository.UpdateMyPassword(data);
     return redirect("/");
   },
 };
 
-export { UserService, formDataToObject, MeService };
+export { UserService, MeService };
